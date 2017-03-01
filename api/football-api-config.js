2adb478fb05e1b4a1f4b2b41e78862f66d6238de
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-03-01 17:13:04 -0500

    Create api folder and move fixtures methods into it

---
+++ api/football-api-config.js
@@ -0,0 +1,9 @@
+module.exports.hostname = 'https://api.football-data.org/v1/';
+module.exports.options = {
+  'method': 'GET',
+  'headers': {
+    'dataType': 'json',
+    'X-Auth-Token': '58be930f357641e8a4cb1fecad504306',
+    'X-Response-Control': 'minified'
+  }
+};
---
+++ api/football-api.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var request = require('request');
+var http = require('http');
+var router = express.Router();
+var config = require('./football-api-config');
+
+module.exports.getMatchesByCompetition = function(id, callback) {
+  var options = config.options;
+  options.url = config.hostname + 'competitions/' + id + '/fixtures?timeFrame=n1';
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(JSON.parse(body).fixtures);
+    }
+    else {
+      console.log(error);
+    }
+  });
+};
+
+module.exports.getMatchesDayBefore = function(callback) {
+  var options = config.options;
+  options.url = config.hostname + '/fixtures?timeFrame=n1';
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(JSON.parse(body).fixtures);
+    }
+    else {
+      console.log(error);
+    }
+  });
+};
+
+module.exports.getMatches = function(callback) {
+  var options = config.options;
+  options.url = config.hostname + '/fixtures?timeFrame=n1';
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(JSON.parse(body).fixtures);
+    }
+    else {
+      console.log(error);
+    }
+  });
+};
+
+module.exports.serveMatches = function serveMatches(collection, ids) {
+  collection.find({}, function(err, document) {
+    if(err) {
+      console.log("ERROR:\n"+err);
+    } else {
+      console.log("SUCCESS:\n"+document);
+    }
+  });
+};
