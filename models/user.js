3741e12b2f8488a0c8a7c02f7f8898ceb0a6b593
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-03-02 18:39:10 -0500

    Create User model

---
+++ models/user.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('User', new Schema({
+    name: String,
+    email: String,
+    password: String,
+    admin: Boolean
+}));
