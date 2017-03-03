d1a59d622028f7388f775831aba7885426ce2b51
Felipe <f.martinez11@uniandes.edu.co>
2017-03-02 21:06:27 -0500

    bootrsrap navbar

--- front/src/components/navbar.js
+++ front/src/components/navbar.js
@@ -1,38 +1,50 @@
-import React, { Component } from 'react';
-import {Navbar,Nav} from 'react-bootstrap/lib/'
+import React, {Component} from 'react';
+import {Nav} from 'react-bootstrap/lib/'
 import '../style/App.css';
-
 
 class Navegacion extends Component {
 
-/*  constructor(props)
+
+
+
+    /*  constructor(props)
   {
     super(props)
   }*/
 
-
   render() {
-    return (
-      <div>
-        <Nav className='navbar navbar-inverse navbar-fixed-top'>
-          <div className='container-fluid'>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="#">FootScores</a>
-              </Navbar.Brand>
-            </Navbar.Header>
-            <ul className="nav navbar-nav">
-              <li className=""><a href="#">Home</a></li>
-              <li><a href="#">Leaderboard</a></li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
-              <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
-            </ul>
+      return (
+          <div>
+              <Nav className="navbar navbar-inverse navbar-fixed-top">
+                  <div className="container-fluid">
+                      <div className="navbar-header">
+                          <a className="navbar-brand" href="#">FootScores</a>
+                      </div>
+                      <ul className="nav navbar-nav">
+                          <li className="active">
+                              <a href="#">Home</a>
+                          </li>
+                          <li>
+                              <a href="#">Leaderboard</a>
+                          </li>
+                      </ul>
+                      <ul className="nav navbar-nav navbar-right">
+                          <li>
+                              <a href="#">
+                                  <span className="glyphicon glyphicon-user"></span>
+                                  Sign Up</a>
+                          </li>
+                          <li>
+                              <a href="#">
+                                  <span className="glyphicon glyphicon-log-in"></span>
+                                  Login
+                              </a>
+                          </li>
+                      </ul>
+                  </div>
+              </Nav>
           </div>
-        </Nav>
-      </div>
-    );
+      );
   }
 }
 export default Navegacion;
