756a8cbb2e97cb28e84387a190dab58e8f52cdd6
Felipe <f.martinez11@uniandes.edu.co>
2017-03-05 20:24:08 -0500

    league logos

--- front/src/components/leagues.js
+++ front/src/components/leagues.js
@@ -1,26 +1,48 @@
 import React, {Component} from 'react';
 import '../style/App.css';
-import Partido from './partido'
+import Partido from './partido';
+import axios from 'axios';
+const URL = "http://localhost:3000";
 class Leagues extends Component {
 
     constructor(props)
     {
         super(props)
-        this.state = {}
+        this.state =
+        {
+          liga:[],
+          lleno: false
+        }
     }
 
 
-
-    postGuess(home,away,date)
+    getFixturesLeague(LeagueCode)
     {
-
-        console.log('entro');
-        console.log(home+' '+away+' '+date);
-
+      console.log(LeagueCode);
+      axios.get(URL+"/fixtures/"+LeagueCode).then(response => {
+        console.log(response.data);
+        if(response.data.length>0)
+        {
+          this.setState(
+          {
+            lleno: true,
+            liga: response.data
+          });
+        }
+        else
+        {this.setState(
+          {
+            liga:[],
+            lleno:false
+          });
+        }
+      });
     }
+
 
     render() {
         var i = 0;
+        const estaLleno = this.state.lleno;
         return (
             <div>
                 <div className='row'>
@@ -30,17 +52,73 @@ class Leagues extends Component {
                 </div>
                 <hr className="content-divider"></hr>
                 <div className='row'>
-                    <div className='col-md-6'>
-                        <h3>Selecciona la competencia</h3>
+                    <div className='col-md-12'>
+                        <h3>Seleccione la competencia</h3>
                     </div>
-                    <div className='col-md-6'></div>
+                </div>
+                <div className='row'>
+                    <div className='col-md-12'>
+                      <div className='row'>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('CL')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/uefa.jpg")}></img>
+                        </div>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('CDR')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/copadelrey.jpg")}></img>
+                        </div>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('BL1')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/bundesliga.jpg")}></img>
+                        </div>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('DED')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/erdivisie.jpg")}></img>
+                        </div>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('FAC')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/FaCup.jpg")}></img>
+                        </div>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('PD')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/ligaSantander.jpg")}></img>
+                        </div>
+                      </div>
+                      <div className='row'>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('FL1')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/ligue1.jpg")}></img>
+                        </div>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('DFB')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/pokal.jpg")}></img>
+                        </div>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('PL')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/premier.jpg")}></img>
+                        </div>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('PPL')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/primeiraLiga.jpg")}></img>
+                        </div>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('SA')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/serieA.jpg")}></img>
+                        </div>
+                        <div className='col-md-2'>
+                          <img onClick={() => this.getFixturesLeague('EL')} alt='logo de la liga' className="img-league img-circle img-responsive"
+                            src={require("../img/leagues/uefacup.jpg")}></img>
+                        </div>
+                      </div>
+                    </div>
                 </div>
                 <hr className="content-divider"></hr>
                 <div className='row'>
-                {this.props.ligaBBVA.map(partido =>{
+                {estaLleno ? (this.state.liga.map(partido =>{
                   i++;
                   return <Partido key={i} partido={partido} />
-                })}
+                }) )
+              : (<h3>No hay partidos disponibles para esta liga, selecciona otra!</h3>)}
                 </div>
             </div>
         );
